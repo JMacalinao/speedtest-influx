bfccd37f62298e79e6b06871f8bac3842e5b1d6e
JJ Macalinao <i@jmac.ph>
2021-05-15 01:23:07 +0800

    Only consider result if upload/download > 0

--- Dockerfile
+++ Dockerfile
@@ -11,7 +11,7 @@ RUN export SPEEDTESTVERSION="1.0.0" && \
     export SPEEDTESTARCH="x86_64" && \
     export SPEEDTESTPLATFORM="linux" && \
     apt-get update && \
-    apt-get install jq curl -y && \
+    apt-get install bc jq curl -y && \
     curl -Ss -L https://ookla.bintray.com/download/ookla-speedtest-$SPEEDTESTVERSION-$SPEEDTESTARCH-$SPEEDTESTPLATFORM.tgz | tar -zx -C /app && \
     chmod +x speedtest
 
--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# Wait for things to calm down for a while
+echo "Wait for things to calm down for a while... (60s)"
 sleep 60
 
 while :; do
@@ -11,6 +11,7 @@ while :; do
  		echo "[$(date)] Starting Speedtest CLI with specific server ID ${SPEEDTEST_SERVER}..."
  		JSON=$(./speedtest --accept-license --accept-gdpr -f json -s ${SPEEDTEST_SERVER})
 	fi
+
 	DOWNLOAD=$(echo ${JSON} | jq -r .download.bandwidth)
 	DOWNLOAD=$(printf %.2f\\n "$((DOWNLOAD * 8 / 10000))e-2")
 	UPLOAD=$(echo ${JSON} | jq -r .upload.bandwidth)
@@ -20,12 +21,18 @@ while :; do
 	SHARE=$(echo ${JSON} | jq -r .result.url)
 	UPLOAD=$(echo $UPLOAD | sed 's/\(\.[0-9][0-9]\)[0-9]*/\1/g')
 	DOWNLOAD=$(echo $DOWNLOAD | sed 's/\(\.[0-9][0-9]\)[0-9]*/\1/g')
-	echo "[$(date)] Speedtest results - Download: ${DOWNLOAD}, Upload: ${UPLOAD}, Ping: ${PING}, Jitter: ${JITTER}, Share: ${SHARE}"
-	curl -sL -XPOST "${INFLUXDB_URL}/write?db=${INFLUXDB_DB}" --data-binary "download,host=${SPEEDTEST_HOST} value=${DOWNLOAD}"
-	curl -sL -XPOST "${INFLUXDB_URL}/write?db=${INFLUXDB_DB}" --data-binary "upload,host=${SPEEDTEST_HOST} value=${UPLOAD}"
-	curl -sL -XPOST "${INFLUXDB_URL}/write?db=${INFLUXDB_DB}" --data-binary "ping,host=${SPEEDTEST_HOST} value=${PING}"
-	curl -sL -XPOST "${INFLUXDB_URL}/write?db=${INFLUXDB_DB}" --data-binary "jitter,host=${SPEEDTEST_HOST} value=${JITTER}"
-	curl -sL -XPOST "${INFLUXDB_URL}/write?db=${INFLUXDB_DB}" --data-binary "share,host=${SPEEDTEST_HOST} value=\"${SHARE}\""
+
+	if (( $(echo "$DOWNLOAD > 0 && $UPLOAD > 0" | bc -l) )); then
+		echo "[$(date)] Speedtest results - Download: ${DOWNLOAD}, Upload: ${UPLOAD}, Ping: ${PING}, Jitter: ${JITTER}, Share: ${SHARE}"
+		curl -sL -XPOST "${INFLUXDB_URL}/write?db=${INFLUXDB_DB}" --data-binary "download,host=${SPEEDTEST_HOST} value=${DOWNLOAD}"
+		curl -sL -XPOST "${INFLUXDB_URL}/write?db=${INFLUXDB_DB}" --data-binary "upload,host=${SPEEDTEST_HOST} value=${UPLOAD}"
+		curl -sL -XPOST "${INFLUXDB_URL}/write?db=${INFLUXDB_DB}" --data-binary "ping,host=${SPEEDTEST_HOST} value=${PING}"
+		curl -sL -XPOST "${INFLUXDB_URL}/write?db=${INFLUXDB_DB}" --data-binary "jitter,host=${SPEEDTEST_HOST} value=${JITTER}"
+		curl -sL -XPOST "${INFLUXDB_URL}/write?db=${INFLUXDB_DB}" --data-binary "share,host=${SPEEDTEST_HOST} value=\"${SHARE}\""
+	else
+		echo "[$(date)] Unable to get Speedtest results."
+	fi
+	
 	echo "[$(date)] Sleeping for ${SPEEDTEST_INTERVAL} seconds..."
 	sleep ${SPEEDTEST_INTERVAL}
 done
